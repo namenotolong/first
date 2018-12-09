aaa2b50357e8f69a5bc5786f485819e6c998f49e
Alex <291242352@qq.com>
2018-12-09 15:46:44 +0800

    commit

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var VueLoaderPlugin = require('vue-loader/lib/plugin');
+module.exports = {
+    mode: 'development',
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [ 
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, './index.html'),
+            filename: 'index.html',
+            minify: {
+                removeAttributeQuotes: true, 
+                removeComments: true, 
+                collapseWhitespace: true, 
+            }
+        }),
+        new VueLoaderPlugin()
+    ],
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        },{
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader','sass-loader']
+        }, {
+            test: /\.jpg|png|gif|jpeg|bmp$/,
+            use: 'url-loader?limit=15000&name=[hash:8]-[name].[ext]'
+            //当图片大小在15000字节以内时，才将图片的路径转化为base64
+        }, {
+            test: /\.ttf|eot|svg|woff|woff2$/,
+            use: 'url-loader'
+        }, {
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: /node_modules/, 
+        }, {
+            test: /\.vue$/,
+            use: 'vue-loader'
+        }]
+    },
+    performance: {
+        hints: "warning", //展示警告
+        maxAssetSize: 500000, // 整数类型（以字节为单位）
+        maxEntrypointSize: 500000, // 整数类型（以字节为单位）
+        assetFilter: function (assetFilename) {
+            // 提供资源文件名的断言函数
+            return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+        }
+    },
+    resolve: {
+        // alias:{
+        //     vue$:'vue/dist/vue.js'  //设置vue被导入时的路径，不然会默认导入vue.runtime.common.js的包。
+        // }
+    }
+};
