83aa1411104ed80c0cd3a1c7a369cfa0c9269e75
Alex <291242352@qq.com>
2018-12-11 10:24:03 +0800

    绘制首页折线图

---
+++ src/pages/Dashboard/LineChart.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="line_chart">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        chartData:[{
+            week:
+        }]
+    };
+  },
+  created() {
+
+  },
+  methods: {
+    createChart() {
+      let chart = new G2.Chart({
+        container: "line_chart",
+        forceFit: true
+      });
+      chart.source()
+    }
+  }
+};
+</script>
+<style scoped>
+#line_chart {
+    width: 100%;
+}
+</style>
