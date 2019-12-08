f895af6047e8091b6b53685b017b8b3426dcb56d
Alex <291242352@qq.com>
2019-12-08 17:28:59 +0800

    修复大小写

--- src/layouts/innerLayout/components/index.js
+++ src/layouts/innerLayout/components/index.js
@@ -1,5 +1,5 @@
-export { default as HeaderBar } from './HeaderBar';
-export { default as SideBar } from './SideBar';
-export { default as NavTag } from './NavTag';
-export { default as Setting } from './Setting';
+export { default as HeaderBar } from './headerBar';
+export { default as SideBar } from './sideBar';
+export { default as NavTag } from './navTag';
+export { default as Setting } from './setting';
 
